d6be76c6a108aa3b27108bf1335357f57feb0866
Rozbitek <rozbitekbachelor@gmail.com>
2017-05-13 15:48:04 +0200

    Adding maze algorithm project

---
+++ MAZE/src/maze/MAZE.java
@@ -0,0 +1,9 @@
+package maze;
+
+public class MAZE{
+
+    public static void main(String[] args){
+        // TODO code application logic here
+    }
+    
+}
---
+++ MAZE/src/maze/Node.java
@@ -0,0 +1,24 @@
+package maze;
+
+public class Node{
+    
+    private int group;
+    
+    public Node(int passedGroup){
+        
+        group = passedGroup;
+        
+    }
+    
+    public void setGroup(int passedGroup){
+        
+        group = passedGroup;
+        
+    }
+    
+    public int getGroup(){
+        
+        return group;
+        
+    }
+}
